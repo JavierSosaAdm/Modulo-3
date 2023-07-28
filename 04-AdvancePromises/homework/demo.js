4785302f58116008f526891aac14a04632ad787b
Javier.pjs <javier.sosa.1911@gmail.com>
2023-07-28 18:10:20 -0300

    demo promesa avanzada

--- 04-AdvancePromises/homework/demo.js
+++ 04-AdvancePromises/homework/demo.js
@@ -6,6 +6,11 @@ const promesa2 = new Promise ((_resolve, reject) => {
     reject('rechazado'); 
 })
 
+const promesa3 = new Promise((resolve, reject) => {
+    resolve('Resuelto 2')
+})
+
+
 // CASO 1 
 // // La promesa se resuelve
 promesa1.then((value) => console.log('caso 1: ', value));
@@ -33,4 +38,83 @@ promesa2 // se rechaza
 .catch((error) => console.log('caso 4: ', error)) // 'caso 4: Rechazado'
 
 // CASO 5
-//
+// La promesa se resuelve A
+
+promesa1
+    .then(
+        (value) => {return 'te paso info'}, // esta promesa se resuelve al valor de 'te paso info'
+        (error) => console.log(error)
+        )  
+    .then((value) => console.log('este es el segundo then: ', value)) // aqui se resuelve la promesa1 porque el 'te paso info se guarda en el value' 
+
+// la promesa se resuelve B
+promesa1
+    .then(
+        (value) => 'te paso info', // esta promesa se resuelve al valor de 'te paso info'
+        (error) => console.log(error)
+        )  
+    .then((value) => console.log('este es el segundo then: ', value)) // esta promesa se resuelve al valor de 'te paso info' por el mismo motivo del caso anterior
+
+// la promesa se resuelve C
+promesa1
+    .then(
+        (value) => console.log('te paso info'), // esta promesa se resuelve al valor de 'te paso info' y se loguea
+        (error) => console.log(error)
+        )  
+    .then((value) => console.log('este es el segundo then: ', value)) // esta promesa se resuelve al valor de 'undefined' puesto que en el primer then la promesa ya logueo el 'te paso info' por lo tanto el value del segundo then no tiene un valor definido.
+
+// CASO 6 
+// promesa se rechaza
+
+promesa2
+        .then(
+            (value) => {return 'otro value'},
+            (error) => {return 'error'} // tenemos una promesa que se rechaza y el 'rechazado'  esta guardado en el error
+            )
+        .then((value) => console.log('caso 6: ', value)) // se muestra el caso 6 porque el error no tiene manejador yt se soluciona así: 
+
+promesa2
+        .then(
+            (value) => {return 'otro value'}, // successHandler
+            (error) => {throw error} // errorHandler NO USAR return       
+        )
+        .then(
+            (value) => console.log('caso 6: ', value),
+            (error) => console.log('errorHandler: ', error) // especificar errorHandler
+        )
+
+// CASO 7:
+
+promesa1
+        .then(
+            (value) => {return promesa3}, // retorna otra promesa que en este caso es la promesa 3 que se resuelve a 'Resuelto2'
+            (error) => {throw error} // los 'throw' lanzan errores hasta que alguien los maneja
+            )
+        .then(
+            (value) => console.log('successHandler: ', value), // se resuelve a 'successHandler: Resuelto2 (el resultado de promesa3)' 
+            (error) => console.log('errorHandler: ', error) // 
+        )
+// si se rechaza A
+
+promesa1
+        .then(
+            (value) => {return promesa2}, // retorna otra promesa que en este caso se rechaza, por lo tanto se resuelve al 'rechazado
+            (error) => {throw error} // lanza error
+        )
+        .then(
+            (value) => console.log('successHandler: ', value),
+            (error) => console.log('errorHandler: ', error) // se resuelve a 'errorHandler: Rechazado' porque promesa2 se resuelve a ese valor
+        )
+
+// si se rechaza B
+
+promesa1
+        .then(
+            (value) => {return promesa2}, // retorna otra promesa que en este caso se rechaza, por lo tanto se resuelve al 'rechazado
+            (error) => {throw error} // lanza error
+        )
+        .then(
+            (value) => console.log('successHandler: ', value),
+            (error) => {throw error} // lanza error nuevamente 
+        )
+        .catch((error) => console.log('errorHandlerB: ', error)) // recibe el error y se resuelve a 'errorHandlerB: Rechazado' porque promesa2 se resuelve a ese valor
